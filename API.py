30d4b4a1b61a40dfbf12a711bdd664f5e062b210
lvzb86 <lvzb86@gmail.com>
2018-09-19 14:18:31 +0800

    添加API相关内容

---
+++ API.py
@@ -0,0 +1,56 @@
+'''
+WebDriverException
+    ElementClickInterceptedException
+    ErrorInResponseException
+    ImeActivationFailedException
+    ImeNotAvailableException
+    InsecureCertificateException
+    InvalidArgumentException
+    InvalidCookieDomainException
+    InvalidCoordinatesException
+    InvalidElementStateException
+        ElementNotInteractableException
+        ElementNotSelectableException
+        ElementNotVisibleException
+    InvalidSessionIdException
+    InvalidSwitchToTargetException
+        NoSuchFrameException
+        NoSuchWindowException
+    JavascriptException
+    MoveTargetOutOfBoundsException
+    NoAlertPresentException
+    NoSuchAttributeException
+    NoSuchCookieException
+    NoSuchElementException
+        InvalidSelectorException
+    RemoteDriverServerException
+    ScreenshotException
+    SessionNotCreatedException
+    StaleElementReferenceException
+    TimeoutException
+    UnableToSetCookieException
+    UnexpectedAlertPresentException
+    UnexpectedTagNameException
+    UnknownMethodException
+'''
+
+
+'''
+Webdriver
+
+    Firefox
+    Chrome
+    Safari
+    IE
+    Edge
+    Opera
+
+    android
+    blackberry
+    webkitgtk
+    phantomjs
+
+    common
+    support
+    remote
+'''
